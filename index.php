7990b67c6496d6bf13a549d38227ae5dcc0eb37f
davidp84 <62162575+davidp84@users.noreply.github.com>
2021-09-16 11:18:00 +1000

    getting core teaching, github & local playing nice

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang='en'>
+
+<?php 
+
+include ('tools.php');
+topModule("The Temp Plotters");
+?>
+
+<h1> DUMMY HEADING <h1>
+
+<p> dummy text </p>
+<p> dummy text </p>
+<p> dummy text </p>
+<p> dummy text </p>
+<p> dummy text </p>
+<p> dummy text </p>
+<p> dummy text </p>
+
+<?= footerModule() ?>
