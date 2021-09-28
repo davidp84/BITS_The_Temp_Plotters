dbb80a18625001151a46c9421d2fd5501dfa0f68
davidp84 <62162575+davidp84@users.noreply.github.com>
2021-09-28 20:42:49 +1000

    Table header. Test CSV File. Table data builder

--- index.php
+++ index.php
@@ -7,15 +7,16 @@ include ('tools.php');
 topModule("The Temp Plotters");
 ?>
 
-<h1> DUMMY HEADING <h1>
+<h1> ALERTS <h1>
 
-<p> dummy text </p>
-<p> dummy text </p>
-<p> dummy text </p>
-<p> dummy text </p>
-<p> dummy text </p>
-<p> dummy text </p>
-<p> dummy text </p>
-<p> Testing update across all redundancies<p>
+<table class="main-table">
+  <tr>
+    <th class="table-header">Date</th>
+    <th class="table-header">Temperature</th>
+    <th class="table-header">Humidity</th>
+    <th class="table-header">Alert Sent</th>
+  </tr>
+    <?= tableModule() ?>
+</table>
 
 <?= footerModule() ?>
